2dd2c78f4fa201aced58caff5e5e1dfa41b885cb
lukiee555 <sonilokesh24@gmail.com>
2020-12-23 15:48:11 +0530

    Assignment-1

--- NodeJS/node_express/index.js
+++ NodeJS/node_express/index.js
@@ -7,20 +7,22 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+const dishRouter = require('./routes/dishRouter');
 app.use('/dishes', dishRouter);
 
-app.use((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+var promoRouter = require('./routes/promoRouter');
+app.use('/promotions', promoRouter.router);
 
-});
+var leaderRouter = require('./routes/leaderRouter');
+app.use('/leadership', leaderRouter.router);
+
 
 const server = http.createServer(app);
-
+app.use(express.static(__dirname + '/public'));
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
